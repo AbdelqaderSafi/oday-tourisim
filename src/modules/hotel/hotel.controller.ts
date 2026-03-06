802c043b4daf977f31cd1ff2cdab520ba59d6e91
Abdelqader <saabood519@gmail.com>
2026-03-06 13:34:55 +0200

    delete dto

--- src/modules/hotel/hotel.controller.ts
+++ src/modules/hotel/hotel.controller.ts
@@ -8,17 +8,17 @@ import {
   Delete,
 } from '@nestjs/common';
 import { HotelService } from './hotel.service';
-import { CreateHotelDto } from './dto/hotel.dto';
-import { UpdateHotelDto } from './dto/update-hotel.dto';
+// import { CreateHotelDto } from './dto/hotel.dto';
+// import { UpdateHotelDto } from './dto/update-hotel.dto';
 
 @Controller('hotel')
 export class HotelController {
   constructor(private readonly hotelService: HotelService) {}
 
-  @Post()
-  create(@Body() createHotelDto: CreateHotelDto) {
-    return this.hotelService.create(createHotelDto);
-  }
+  // @Post()
+  // create(@Body() createHotelDto: CreateHotelDto) {
+  //   return this.hotelService.create(createHotelDto);
+  // }
 
   @Get()
   findAll() {
@@ -30,10 +30,10 @@ export class HotelController {
     return this.hotelService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateHotelDto: UpdateHotelDto) {
-    return this.hotelService.update(+id, updateHotelDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateHotelDto: UpdateHotelDto) {
+  //   return this.hotelService.update(+id, updateHotelDto);
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
--- src/modules/hotel/hotel.service.ts
+++ src/modules/hotel/hotel.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { CreateHotelDto } from './dto/hotel.dto';
-import { UpdateHotelDto } from './dto/update-hotel.dto';
+// import { CreateHotelDto } from './dto/hotel.dto';
+// import { UpdateHotelDto } from './dto/update-hotel.dto';
 
 @Injectable()
 export class HotelService {
-  create(createHotelDto: CreateHotelDto) {
-    return 'This action adds a new hotel';
-  }
+  // create(createHotelDto: CreateHotelDto) {
+  //   return 'This action adds a new hotel';
+  // }
 
   findAll() {
     return `This action returns all hotel`;
@@ -16,9 +16,9 @@ export class HotelService {
     return `This action returns a #${id} hotel`;
   }
 
-  update(id: number, updateHotelDto: UpdateHotelDto) {
-    return `This action updates a #${id} hotel`;
-  }
+  // update(id: number, updateHotelDto: UpdateHotelDto) {
+  //   return `This action updates a #${id} hotel`;
+  // }
 
   remove(id: number) {
     return `This action removes a #${id} hotel`;
